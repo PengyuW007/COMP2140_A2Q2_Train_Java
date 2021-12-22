dd173e9504808a507507900cc29f679894431e4a
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2021-12-22 00:35:03 -0600

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan;
+        String filename;
+
+        scan = new Scanner(System.in);
+        System.out.println("\nPlease enter the input file name: ");
+        filename = scan.nextLine();
+
+        processFile(filename);
+
+    }//end main
+
+    public static void processFile(String filename) {
+        BufferedReader inFile;
+        String nextLine;
+        String tokens[];
+
+        System.out.println("Process file " + filename + "...");
+
+        try{
+            inFile = new BufferedReader(new FileReader(filename));
+            nextLine = inFile.readLine();
+
+            while(nextLine!=null){
+                
+            }
+
+        }catch (Exception e){
+
+        }//end try-catch
+    }
+}
